e952d4e3eb02eb5ceaedb8c28ac6a4b454c381f9
roypeer <roypeer11@gmail.com>
2018-06-02 00:07:19 +0300

    Add files via upload

---
+++ failed3Dprintsassistant.py
@@ -0,0 +1,88 @@
+########### FAILED 3D PRINTS RESCUER ########### 
+# Tool to fix gcode
+# Written by Roy Pe'er, 2018
+
+'''
+gcode = open('car.txt','r')
+gcode = gcode.readlines()
+
+for i in range(len(gcode)):
+    if('Z24' in gcode[i]):
+            print(i)
+
+
+gcode = gcode[112615:]
+newcode = open("newcode200.txt",'w')
+newcode.writelines(gcode)
+newcode.close()
+'''
+
+
+
+open_screen = '''
+
+  ______    _ _          _   ____  _____    _____      _       _                          _ _              _   
+ |  ____|  (_) |        | | |___ \|  __ \  |  __ \    (_)     | |           /\           (_) |            | |  
+ | |__ __ _ _| | ___  __| |   __) | |  | | | |__) | __ _ _ __ | |_ ___     /  \   ___ ___ _| |_ __ _ _ __ | |_ 
+ |  __/ _` | | |/ _ \/ _` |  |__ <| |  | | |  ___/ '__| | '_ \| __/ __|   / /\ \ / __/ __| | __/ _` | '_ \| __|
+ | | | (_| | | |  __/ (_| |  ___) | |__| | | |   | |  | | | | | |_\__ \  / ____ \\__ \__ \ | || (_| | | | | |_ 
+ |_|  \__,_|_|_|\___|\__,_| |____/|_____/  |_|   |_|  |_|_| |_|\__|___/ /_/    \_\___/___/_|\__\__,_|_| |_|\__|
+                                                                                                               
+ ##############################################################################################################                                                                                                              
+                                                                                                          
+'''
+
+print(open_screen)
+
+
+
+gcode_loc = input("Enter location of original GCODE (save as txt file !): ")
+gcode = open(str(gcode_loc),'r')
+gcode = gcode.readlines()
+
+approx_z = float(input("Enter approximated Z value where print stopped: "))
+z_vals_location = []
+for i in range(len(gcode)):
+    if(('Z' + str(int(approx_z-1))) in gcode[i]):
+            z_val = gcode[i][gcode[i].find('Z'):]
+            z_val = z_val[:z_val.find(' ')]
+            z_vals_location.append((z_val,i)) # Location and Z values.
+    if(('Z' + str(int(approx_z))) in gcode[i]):
+            z_val = gcode[i][gcode[i].find('Z'):]
+            z_val = z_val[:z_val.find(' ')]
+            z_vals_location.append((z_val,i)) # Location and Z values.
+    if(('Z' + str(int(approx_z+1))) in gcode[i]):
+            z_val = gcode[i][gcode[i].find('Z'):]
+            z_val = z_val[:z_val.find(' ')]
+            z_vals_location.append((z_val,i)) # Location and Z values.
+
+
+#print(z_vals_location)
+
+print("\nHere are some possible initial Z values to restart the print with: ")
+for i in range(len(z_vals_location)):
+    print( str(i + 1) +  '. ' + str(z_vals_location[i][0]))
+
+choice = input('Enter the number of initial Z value you would like to use: ')
+
+z_val_andloc = z_vals_location[int(choice) - 1]
+
+#print(z_val)
+
+def new_gcode(current_gcode,loc,newfilename):
+    current_gcode = current_gcode[loc:]
+    newcode = open(newfilename + '.txt','w')
+    newcode.writelines(current_gcode)
+    newcode.close()
+
+
+filename = input("How would you like the new gcode file to be named ? ")
+new_gcode(gcode,z_val_andloc[1],filename)
+print('Saved new gcode starting from z height ' + z_val_andloc[0] + ' as a txt file called ' + filename + '.txt')
+print('Warm up your printer, make sure it is aware of its location (Home all axis if needed), copy the new gcode to the host software and continue the print ')
+print('Good Luck !')
+
+
+
+
+
